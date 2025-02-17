5a88d984b89db63dc67bb5ba34b52fd13a93a1bc
zygoballus <119077481+zygoballus@users.noreply.github.com>
2025-02-17 17:14:57 -0600

    Adding another test

--- mineralogytest.php
+++ mineralogytest.php
@@ -1,14 +1,24 @@
 <?php
-error_reporting( E_ALL );
-ini_set( 'display_errors', 1 );
-ini_set( 'max_execution_time', 120 );
-
 require_once './imu/lib/IMu.php';
 require_once IMu::$lib . '/Session.php';
+$mainServer = "208.103.112.150";
 
-//$session = new IMuSession('208.103.112.159', 40233);
-$session = new IMuSession('208.103.112.150', 40233);
-$session->connect();
+$session = new IMuSession;
+$session->host = $mainServer;
+try
+{
+    $session->connect();
+}
+catch (IMuException $e)
+{
+    // Check for specific SessionConnect error
+    if ($e->id != 'SessionConnect')
+    {
+        echo "Error: $e";
+        exit(1);
+    }
+}
+echo "Connection successful.<br>\n";
  
 require_once IMu::$lib . '/Module.php';
 require_once IMu::$lib . '/Terms.php';
@@ -21,4 +31,3 @@ $search->add('CatDepartment', 'Mineralogy');
 $hits = $catalogue->findTerms($search);
 printf($hits);
 exit(0);
-
